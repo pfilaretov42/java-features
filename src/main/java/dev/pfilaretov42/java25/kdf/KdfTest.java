6b1a0a114b2e4a9731edf266188f10f8f87f159a
pfilaretov42 <petr.filaretov@gmail.com>
2025-07-09 10:05:27 -0700

    update Key Derivation Function API

--- src/main/java/dev/pfilaretov42/java25/kdf/KdfTest.java
+++ src/main/java/dev/pfilaretov42/java25/kdf/KdfTest.java
@@ -5,47 +5,45 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.HKDFParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
-import java.util.Base64;
 import java.util.HexFormat;
 
 public class KdfTest {
     void main() throws GeneralSecurityException {
         before();
-        afterSven();
+        after();
     }
 
     private void before() throws GeneralSecurityException {
-        // Like forging a sword in the crude fires of Mordor
-        String password = "the super secret passphrase";
+        String password = "Mellon!";
         byte[] salt = generateSalt();
         int iterations = 65_536;
-        int keyLength = 256;
+        int keyLengthBits = 256;
 
-        // derive the key - PBKDF2 with homemade parameters
-        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, keyLength);
+        // derive the key
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, keyLengthBits);
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
         SecretKey key = factory.generateSecret(spec);
 
         System.out.println("before - derived key: " + HexFormat.of().formatHex(key.getEncoded()));
     }
 
-    private void afterSven() throws GeneralSecurityException {
+    private void after() throws GeneralSecurityException {
         KDF hkdf = KDF.getInstance("HKDF-SHA256");
-        byte[] password = "the super secret passphrase".getBytes();
+        byte[] password = "Mellon!".getBytes();
         byte[] salt = generateSalt();
-        byte[] info = "my derived key".getBytes();
-        int length = 32;
+        byte[] info = "Say 'Friend' and enter".getBytes();
+        int keyLengthBytes = 32;
 
+        // derive the key
         AlgorithmParameterSpec params = HKDFParameterSpec.ofExtract()
                 .addIKM(password)
                 .addSalt(salt)
-                .thenExpand(info, length);
-
+                .thenExpand(info, keyLengthBytes);
         SecretKey key = hkdf.deriveKey("AES", params);
+
         System.out.println("after - derived key: " + HexFormat.of().formatHex(key.getEncoded()));
     }
 
